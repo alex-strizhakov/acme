032457ad99135c1b47d46f3e554eeb27317a820f
Alexander Strizhakov <alex.d3vils@gmail.com>
2022-09-18 13:16:24 +0400

    challeng plug debug

--- lib/acme/challenge_plug.ex
+++ lib/acme/challenge_plug.ex
@@ -2,6 +2,8 @@ defmodule Acme.ChallengePlug do
   @behaviour Plug
   import Plug.Conn
 
+  require Logger
+
   @impl true
   def init(opts) do
     Keyword.get(opts, :acme_client, Acme.Client)
@@ -9,11 +11,20 @@ defmodule Acme.ChallengePlug do
 
   @impl true
   def call(conn, client) do
+    Logger.warn("request for #{conn.request_path}")
+
     case conn.request_path do
       "/.well-known/acme-challenge/" <> token ->
+        Logger.warn("started challenge for #{token}")
+
         case Acme.Client.challenge(token, client) do
-          {:ok, authorization} -> conn |> send_resp(200, authorization) |> halt()
-          {:error, _} -> conn |> send_resp(404, "Not found") |> halt()
+          {:ok, authorization} ->
+            Logger.warn("challenge success")
+            conn |> send_resp(200, authorization) |> halt()
+
+          {:error, _} = error ->
+            Logger.error("challenge error #{inspect(error)}")
+            conn |> send_resp(404, "Not found") |> halt()
         end
 
       _ ->
